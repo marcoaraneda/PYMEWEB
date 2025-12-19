19ba877cab9bfea0bc110000c5a75ddd20b6d270
marko2blea <marco.araneda.rodriguez23@gmail.com>
2025-12-19 18:15:13 -0300

    l

--- apps/catalogo/urls_public.py
+++ apps/catalogo/urls_public.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views_public import CategoryPublicListAPIView, ProductPublicListAPIView, ProductPublicDetailAPIView
+from apps.catalogo.views import ProductListAPIView
 
 urlpatterns = [
     path("categories/", CategoryPublicListAPIView.as_view(), name="categories-public"),
     path("products/", ProductPublicListAPIView.as_view(), name="products-public"),
     path("products/<slug:slug>/", ProductPublicDetailAPIView.as_view(), name="product-public-detail"),
+    path('productos/', ProductListAPIView.as_view(), name='public-product-list'),
 ]
+
